2c31ef647601ccb340b19e27c8ccfec5141d1d09
汝吾此便安好 <sonder.qicat@outlook.com>
2026-04-15 22:05:11 +0800

    添加插件

--- lua/community.lua
+++ lua/community.lua
@@ -6,5 +6,13 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  -- 前端全家桶
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },         -- 自动处理 Volar 和 TS
+  { import = "astrocommunity.pack.html-css" },    -- 包含 SCSS 和 Emmet
+  { import = "astrocommunity.formatting.prettier" }, -- 代码格式化
+  
+  -- 后端 Rust 环境 (自动配置 rust-analyzer)
+  { import = "astrocommunity.pack.rust" },
   -- import/override with your plugins folder
 }
